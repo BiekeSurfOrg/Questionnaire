0aaa6b511f425826bf8e48ddebf19b142123c58d
BorisStankov98 <boris.stankovv@gmail.com>
2025-01-27 15:44:19 +0200

    add background video and adjust layout styling

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,10 @@ import Card from './Card';
 const Home = () => {
 
     return (
-        <div className="full-screen">
+        <div className="full-screen position-relative">
+            <video autoPlay muted loop id="myVideo" className='background-video'>
+                <source src="../assets/background.mp4" type="video/mp4" />
+            </video>
             <div className="full-screen layout">
                 <div className='tile-list'>
                     <Card text='Cyber Fraude Test' route='/questions?type=cybercrime' />
